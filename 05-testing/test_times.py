c84bf6d0836e449ad8b4da2bf11aecdaeec1775a
EricCWWong <ericwongchunwing@gmail.com>
2019-10-31 11:39:08 +0000

    fixed large

--- 05-testing/test_times.py
+++ 05-testing/test_times.py
@@ -8,8 +8,8 @@ def test_given_input():
     assert result == expected
 
 def test_class_time():
-    large = time_range("2010-01-12 10:05:00", "2010-01-12 12:55:00", 3, 600)
-    short = time_range("2010-01-12 10:10:00", "2010-01-12 10:30:00")
+    large = time_range("2010-01-12 09:05:00", "2010-01-12 13:55:00")
+    short = time_range("2010-01-12 10:05:00", "2010-01-12 12:55:00", 3, 600)
     result = overlap_time(large, short)
     expected = short
     assert result == expected
